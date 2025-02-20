9588930d2b5346676910a69ac8e3c7719273b167
Mica CF <mcohenfuentes@gmail.com>
2025-02-20 12:51:39 -0500

    post route for applications

--- controllers/applications.js
+++ controllers/applications.js
@@ -15,4 +15,28 @@ router.get('/', async (req, res) => {
       }
 })
 
+//GET /users/:userId/applications/new
+router.get('/new', async (req, res) => {
+    res.render('applications/new.ejs');
+  });
+
+//POST /users/:userId/applications
+router.post('/', async (req, res) => {
+    try {
+      // Look up the user from req.session
+      const currentUser = await User.findById(req.session.user._id);
+      // Push req.body (the new form data object) to the
+      // applications array of the current user
+      currentUser.applications.push(req.body);
+      // Save changes to the user
+      await currentUser.save();
+      // Redirect back to the applications index view
+      res.redirect(`/users/${currentUser._id}/applications`);
+    } catch (error) {
+      // If any errors, log them and redirect back home
+      console.log(error);
+      res.redirect('/');
+    }
+  });
+
 module.exports = router;
