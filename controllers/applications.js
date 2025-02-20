c5546f83911ef2957bc797bcaddf80f1bf6dbecb
Mica CF <mcohenfuentes@gmail.com>
2025-02-20 13:03:48 -0500

    added apps to index page

--- controllers/applications.js
+++ controllers/applications.js
@@ -8,12 +8,16 @@ const User = require('../models/user.js')
 // index route
 router.get('/', async (req, res) => {
     try {
-        res.render('applications/index.ejs');
-    } catch (error) {
+        const currentUser = await User.findById(req.session.user._id);
+       
+        res.render('applications/index.ejs', {
+          applications: currentUser.applications,
+        });
+      } catch (error) {
         console.log(error);
         res.redirect('/');
       }
-})
+    });
 
 //GET /users/:userId/applications/new
 router.get('/new', async (req, res) => {
@@ -23,17 +27,16 @@ router.get('/new', async (req, res) => {
 //POST /users/:userId/applications
 router.post('/', async (req, res) => {
     try {
-      // Look up the user from req.session
+     
       const currentUser = await User.findById(req.session.user._id);
-      // Push req.body (the new form data object) to the
-      // applications array of the current user
+      
       currentUser.applications.push(req.body);
-      // Save changes to the user
+      
       await currentUser.save();
-      // Redirect back to the applications index view
+      
       res.redirect(`/users/${currentUser._id}/applications`);
     } catch (error) {
-      // If any errors, log them and redirect back home
+
       console.log(error);
       res.redirect('/');
     }
